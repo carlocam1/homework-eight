1b41d2524930d856c01ffe6f16102ed0eaa0a2b1
carlocam1 <114327906+carlocam1@users.noreply.github.com>
2022-10-07 23:15:15 -0400

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,300 @@
+var homeContent = `<div class="home">
+<div class="hero">
+  <div class="hero-image picture">
+    <div class="callout">
+      <h1>PDM</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Architecto, sunt saepe reiciendis consequatur totam . Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. Architecto,
+        sunt saepe reiciendis consequatur totam .Lorem ipsum dolor sit
+        amet consectetur adipisicing elit. Architecto, sunt saepe
+        reiciendis consequatur totam .
+      </p>
+    </div>
+  </div>
+</div>
+<div class="products">
+  <div class="services">
+    <h1>Painting</h1>
+    <img src="images/panting.jpg" alt="" />
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+      iure rem ipsum aliquam architecto culpa, maxime, cupiditate
+      possimus aperiam odit cumque amet adipisci asperiores! Dolores!
+    </p>
+  </div>
+  <div class="services">
+    <h1>Design</h1>
+    <img src="images/design.jpg" alt="" />
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+      iure rem ipsum aliquam architecto culpa, maxime, cupiditate
+      possimus aperiam odit cumque amet adipisci asperiores! Dolores!
+    </p>
+  </div>
+  <div class="services">
+    <h1>3D Modeling</h1>
+    <img src="images/3d-modeling.jpg" alt="" />
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+      iure rem ipsum aliquam architecto culpa, maxime, cupiditate
+      possimus aperiam odit cumque amet adipisci asperiores! Dolores!
+    </p>
+  </div>
+</div>
+<div class="comments picture">
+  <div class="comments-callout">
+    <h1>Costumer Satisfaction</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Cupiditate adipisci provident maxime repudiandae voluptates
+      veritatis cumque? Aliquam, fuga nesciunt eveniet, voluptatum,
+      dolorem eaque odio itaque cumque magni exercitationem
+      necessitatibus! Voluptatem eum, aspernatur, nesciunt saepe
+      explicabo, asperiores fuga totam aperiam quia voluptates alias
+      minus consequuntur? Ut deleniti esse dolorem iure quos.
+    </p>
+  </div>
+</div>
+</div>`; 
+
+var aboutContent = ` <div class="about">
+<div class="hero">
+  <div class="hero-image picture">
+    <div class="callout">
+      <h1>about...Awesome people</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Architecto, sunt saepe reiciendis consequatur totam . Lorem
+        dolor sit amet consectetur adipisicing elit. Architecto, sunt
+        saepe reiciendis consequatur totam .
+      </p>
+    </div>
+  </div>
+</div>
+<div class="owners">
+  <div class="owner-container">
+    <div class="own-left">
+      <img src="images/person1.jpg" alt="" />
+    </div>
+    <div class="own-right">
+      <h1>Caitlin</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi
+        iure aspernatur totam voluptas nam exercitationem iusto ipsum.
+        Provident laudantium neque beatae voluptatem sequi consequatur
+        eius perferendis repellendus id eligendi recusandae quos
+        aspernatur, fugit sapiente culpa eum dolores, accusamus,
+        voluptatibus laboriosam accusantium! Totam rem quos, earum
+        excepturi nam qui? Tenetur, non! "THI IS HOW I WANT THIS TO LOOK
+        WHEN RESIZING THE SCREEN"
+      </p>
+    </div>
+  </div>
+  <div class="owner-container">
+    <div class="own-left">
+      <img src="images/person2.jpg" alt="" />
+    </div>
+    <div class="own-right">
+      <h1>Kayla</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi
+        iure aspernatur totam voluptas nam exercitationem iusto ipsum.
+        Provident laudantium neque beatae voluptatem sequi consequatur
+        eius perferendis repellendus id eligendi recusandae quos
+        aspernatur, fugit sapiente culpa eum dolores, accusamus,
+        voluptatibus laboriosam accusantium! Totam rem quos, earum
+        excepturi nam qui? Tenetur, non!
+      </p>
+    </div>
+  </div>
+  <div class="owner-container">
+    <div class="own-left">
+      <img src="images/person3.jpg" alt="" />
+    </div>
+    <div class="own-right">
+      <h1>Karen</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi
+        iure aspernatur totam voluptas nam exercitationem iusto ipsum.
+        Provident laudantium neque beatae voluptatem sequi consequatur
+        eius perferendis repellendus id eligendi recusandae quos
+        aspernatur, fugit sapiente culpa eum dolores, accusamus,
+        voluptatibus laboriosam accusantium! Totam rem quos, earum
+        excepturi nam qui? Tenetur, non!
+      </p>
+    </div>
+  </div>
+</div>
+<div class="comments picture">
+  <div class="about-image">
+    <img src="images/panting.jpg" alt="" />
+  </div>
+  <div class="text">
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum quas
+      vel dolorem, quasi autem quae quam praesentium quaerat! Impedit
+      ducimus commodi voluptatibus praesentium sed cum voluptate atque
+      sint dicta consequatur, quo voluptas dolores beatae nam, quam
+      molestiae aperiam delectus,
+    </p>
+  </div>
+</div>
+</div>`;
+
+var productsContent = ` <div class="item-product">
+<div class="hero">
+  <div class="hero-image picture">
+    <div class="callout">
+      <h1>Products & services</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Architecto, sunt saepe reiciendis consequatur totam . Lorem
+        dolor sit amet consectetur adipisicing elit. Architecto, sunt
+        saepe reiciendis consequatur totam .
+      </p>
+    </div>
+  </div>
+</div>
+<div class="wrapper">
+  <div class="item">
+    <h1>P-Style#1</h1>
+    <img src="images/1.jpg" alt="" />
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit
+      dolor praesentium voluptatum facere cupiditate qui? Veniam amet
+      ratione nam, explicabo rem in vitae, ipsam sit commodi tempora
+      sint. Corrupti, vel.
+    </p>
+  </div>
+  <div class="item">
+    <h1>P-Style#2</h1>
+    <img src="images/2.jpg" alt="" />
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit
+      dolor praesentium voluptatum facere cupiditate qui? Veniam amet
+      ratione nam, explicabo rem in vitae, ipsam sit commodi tempora
+      sint. Corrupti, vel.
+    </p>
+  </div>
+  <div class="item">
+    <h1>P-Style#3</h1>
+    <img src="images/3.jpg" alt="" />
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit
+      dolor praesentium voluptatum facere cupiditate qui? Veniam amet
+      ratione nam, explicabo rem in vitae, ipsam sit commodi tempora
+      sint. Corrupti, vel.
+    </p>
+  </div>
+  <div class="item">
+    <h1>D-Style#1</h1>
+
+    <img src="images/4.jpg" alt="" />
+
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit
+      dolor praesentium voluptatum facere cupiditate qui? Veniam amet
+      ratione nam, explicabo rem in vitae, ipsam sit commodi tempora
+      sint. Corrupti, vel.
+    </p>
+  </div>
+  <div class="item">
+    <h1>D-Style#2</h1>
+    <img src="images/5.jpg" alt="" />
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit
+      dolor praesentium voluptatum facere cupiditate qui? Veniam amet
+      ratione nam, explicabo rem in vitae, ipsam sit commodi tempora
+      sint. Corrupti, vel.
+    </p>
+  </div>
+  <div class="item">
+    <h1>D-Style#3</h1>
+    <img src="images/6.jpg" alt="" />
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit
+      dolor praesentium voluptatum facere cupiditate qui? Veniam amet
+      ratione nam, explicabo rem in vitae, ipsam sit commodi tempora
+      sint. Corrupti, vel.
+    </p>
+  </div>
+</div>
+</div>`;
+
+var contactContent = ` <div class="contact">
+<div class="hero">
+  <div class="hero-image picture">
+    <div class="callout">
+      <h1>Contact US...</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Architecto, sunt saepe reiciendis consequatur totam . Lorem
+        dolor sit amet consectetur adipisicing elit. Architecto, sunt
+        saepe reiciendis consequatur totam .
+      </p>
+    </div>
+  </div>
+</div>
+
+<div class="social-media">
+  <div class="media">
+    <img src="images/snapchat-logo.jpg" alt="" />
+  </div>
+  <div class="media">
+    <img src="images/tiktok.jpg" alt="" />
+  </div>
+  <div class="media">
+    <img src="images/instagram.jpg" alt="" />
+  </div>
+</div>
+
+<div class="form">
+  <div class="contact-form">
+    <div class="comments">
+      <textarea
+        name="Message"
+        id=""
+        cols="30"
+        rows="10"
+        placeholder="Message"
+      ></textarea>
+    </div>
+    <div class="input-form">
+      <input type="text" name="Name" id="name" placeholder="Name" />
+      <input
+        type="text"
+        name="Last Name"
+        id="lastName"
+        placeholder="Last Name"
+      />
+      <input type="text" name="Email" id="email" placeholder="Email" />
+      <input
+        type="text"
+        name="Phone"
+        id=""
+        placeholder="Phone (optional)"
+      />
+    </div>
+  </div>
+  <div class="btn">Submit</div>
+</div>
+</div>`;
+   
+
+function initListeners (){
+    $("nav a").click(function(e){
+      let aID = e.currentTarget.id;
+      let contentID = aID + "Content";
+    //   console.log((contentID));  
+
+    $("#app").html(eval(contentID));
+    });
+}
+
+
+$(document).ready(function(){
+    initListeners();
+    $("#app").html(homeContent);
+});
